71352a9cf53701876ad489c47f2c7a58e24ff6ff
webweaver101 <1511842@rgu.ac.uk>
2017-04-21 00:44:02 +0100

    style

--- simp.php
+++ simp.php
@@ -1,8 +1,8 @@
 <?php
 //Simple PHP File Manager
 //Copyright John Campbell (jcampbell1)
-//Liscense: MIT
-// ********************************/
+//License: MIT
+// Modified (slightly) by Bill Laing
 //Disable error report for undefined superglobals
 error_reporting( error_reporting() & ~E_NOTICE );
 //Security options
