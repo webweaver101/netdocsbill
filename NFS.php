d563589ec1fb77a878f83228e5f511b05f4d9314
webweaver101 <1511842@rgu.ac.uk>
2017-04-20 22:17:00 +0100

    style

--- NFS.php
+++ NFS.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-    print_r($_SESSION);
+    //print_r($_SESSION);
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false) {
     echo "You  need  to  login ";
     header('Location: index.html');
@@ -12,7 +12,7 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/home/site/wwwroo
 echo "Cookie '" . $cookie_name . "' is set!<br>";
 echo "Value is: " . $_COOKIE[$cookie_name];
 $_SESSION['loggedin'] = false;
-    print_r($_SESSION);
+    //print_r($_SESSION);
 //define('URL', '/home/site/private/');
 //echo (URL);
 //header('Location: '.URL.'simp.php');
