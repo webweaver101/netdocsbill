83a04a1a5ec00d9248d7d1385a33ceeb5509668a
webweaver101 <1511842@rgu.ac.uk>
2017-04-20 22:08:04 +0100

    style

--- NFS.php
+++ NFS.php
@@ -9,12 +9,13 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == false) {
 $cookie_name = $username;
 $cookie_value = "Any User";
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/home/site/wwwroot/Customers"); // 86400 = 1 day
+echo $cookie_name
 $_SESSION['loggedin'] = false;
     //print_r($_SESSION);
 //define('URL', '/home/site/private/');
 //echo (URL);
 //header('Location: '.URL.'simp.php');
-header('Location: simp.php');
+//header('Location: simp.php');
 
 
 
