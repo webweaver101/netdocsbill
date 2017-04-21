4c8c327d6c1bb9e5ae61307dff46a1fb1f16bdaa
webweaver101 <1511842@rgu.ac.uk>
2017-04-21 06:36:15 +0100

    style

--- oops.php
+++ oops.php
@@ -9,8 +9,8 @@
 
 <h1>Do you want to register, or change your password?</h1>
 
-<p>Please contact the system admin who will be pleased to help
-    Tel 01224 515167.</p>
+<p>Please contact the system admin who will be pleased to help.
+    Tel. 01224 515167.</p>
 
 </body>
 </html>
